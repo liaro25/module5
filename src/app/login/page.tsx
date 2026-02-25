0df00959f515b1951d8df11b9483525824be34e6
liaro25 <lia.rosliany@gmail.com>
2026-02-25 21:54:40 +0900

    chore: tweak login page

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -2,7 +2,7 @@ import LoginForm from "@/app/ui/login-form";
 
 export default function LoginPage() {
   return (
-    <div className="min-h-screen flex items-center justify-center">
+    <div className="h-full grid place-items-center px-4">
       <div className="w-full max-w-md p-8">
         <h1 className="text-3xl font-bold text-center mb-6">Login</h1>
         <LoginForm />
